00001d753a6da636c86478be376a88f2b984bbb9
Tiffany Trinh <tttrinh@ucsd.edu>
2018-02-22 21:14:52 -0800

    templtes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
-  res.render("index");
+var data = require('../data.json');
+
+exports.view = function(req, res) {
+  res.render('index', data);
 };
