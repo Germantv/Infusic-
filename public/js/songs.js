1cfee7097203464550cac6ce8a1dd602d2fccb2d
Tiffany Trinh <tttrinh@ucsd.edu>
2018-03-01 03:48:25 -0800

    fixed ooz

--- public/js/songs.js
+++ public/js/songs.js
@@ -1,16 +1,19 @@
+audioPlayer();
 function audioPlayer(){
-            var currentSong = 0;
+            //var currentSong = 0;
             $("#audioPlayer")[0].src = $("#playlist li a")[0];
             $("#audioPlayer")[0].play();
+            
             $("#playlist li a").click(function(e){
                e.preventDefault(); 
+
                $("#audioPlayer")[0].src = this;
                $("#audioPlayer")[0].play();
                $("#playlist li").removeClass("current-song");
                 currentSong = $(this).parent().index();
                 $(this).parent().addClass("current-song");
             });
-            
+           
             $("#audioPlayer")[0].addEventListener("ended", function(){
                currentSong++;
                 if(currentSong == $("#playlist li a").length)
@@ -19,5 +22,10 @@ function audioPlayer(){
                 $("#playlist li:eq("+currentSong+")").addClass("current-song");
                 $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
                 $("#audioPlayer")[0].play();
+
             });
         }
+
+function goBack() {
+    window.history.back();
+  }
