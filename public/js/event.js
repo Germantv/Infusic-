03bd954337bd672dade3d3467803b4f443c056e7
German Flores <gfloresa@ucsd.edu>
2018-03-08 00:23:19 -0800

    fuck cs

---
+++ public/js/event.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function intializePage() {
+	$('#btn1, #btn2, #btn3').click(function() {
+		ga('send', 'event', 'songs', 'click');
+	});
+	$('#b1, #b2, #b3, #b4, #b5').click(function() {
+		ga('send', 'event', 'button', 'click');		
+	});
+}
+
