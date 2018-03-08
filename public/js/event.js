c6e6956bf6839c82709a92a6af63d7ddb22104c5
German Flores <gfloresa@ucsd.edu>
2018-03-08 15:47:34 -0800

    fixed google analytics

--- public/js/event.js
+++ public/js/event.js
@@ -3,17 +3,25 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	initialize();
 });
+
+
+
+$('#btn1, #btn2, #btn3').click(initialize);
+$('#b1, #b2, #b3, #b4, #b5').click(initializePage);
+
+function initialize(event) {
+	console.log('song clicked');
+	ga('send','event','song','click');
+}
+
+function initializePage(event) {
+	console.log('button clicked');
+	ga('send','event','button','click');
+}
 
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$('#btn1, #btn2, #btn3').click(function() {
-		ga('send', 'event', 'songs', 'click');
-	});
-	$('#b1, #b2, #b3, #b4, #b5').click(function() {
-		ga('send', 'event', 'button', 'click');		
-	});
-}
-
+ 
